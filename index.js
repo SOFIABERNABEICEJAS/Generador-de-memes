89142184326d443ea582c47a72c8abe619793ed1
unknown <sofiabernabeicejas@gmail.com>
2021-08-04 18:04:29 -0300

    No me aparece lo que guarde ayer y hoy

---
+++ index.js
@@ -0,0 +1,8 @@
+
+let botonImagen =document.elementById("boton-imagen")
+let formularioImage = document.querySelector(".formulario-imagen-derecha")
+
+botonImagen.onclick=()=>{
+    .formulario-imagen-derecha.classList=(".mostrar")
+
+}
