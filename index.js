b47b56a08821670fc519e293571fda9aec8998e5
unknown <sofiabernabeicejas@gmail.com>
2021-08-09 12:48:50 -0300

    comienzo con js, imagen- texto- modoclaro-modo oscuro

--- index.js
+++ index.js
@@ -1,8 +1,61 @@
 
-let botonImagen =document.elementById("boton-imagen")
-let formularioImage = document.querySelector(".formulario-imagen-derecha")
+let botonImagen =document.getElementById("boton-imagen")
+let seccionImagen=document.getElementById("formulario-imagen")
 
-botonImagen.onclick=()=>{
-    formulario-imagen-derecha.classList(".mostrar")
+let mostrarImagen =()=>{    
+seccionImagen.style.display="flex"
+seccionTexto.style.display="none"
 
+}
+botonImagen.onclick = mostrarImagen
+
+
+let botonCerrarImagen = document.getElementById("boton-cerrar-imagen")
+
+let cerrarImagen=()=>{
+ seccionImagen.style.display="none"
+
+}
+botonCerrarImagen.onclick=cerrarImagen
+
+
+let botonTexto = document.getElementById("boton-texto")
+let seccionTexto=document.getElementById("formulario-texto")
+
+let mostrarTexto=()=>{
+ seccionTexto.style.display="flex"
+ seccionImagen.style.display="none"
+
+}
+botonTexto.onclick = mostrarTexto
+
+let botonCerrarTexto= document.getElementById("boton-cerrar-texto")
+
+let cerrarTexto=()=>{
+  seccionTexto.style.display="none"
+  
+}
+botonCerrarTexto.onclick=cerrarTexto
+
+
+let botonModoOscuro= document.getElementById("boton-modo-oscuro")
+let botonModoClaro=document.getElementById("boton-modo-claro")
+let ocultar=document.querySelector(".ocultar")
+let mostrar=document.querySelector(".mostrar")
+
+
+botonModoOscuro.onclick=()=>{
+  if(botonModoClaro.style.display="flex"){
+    botonModoOscuro.style.display="none"
+  }
+  
+}
+
+
+
+botonModoClaro.onclick=()=>{
+  if(botonModoOscuro.style.display="flex"){
+    botonModoClaro.style.display="none"
+  }
+  
 }
