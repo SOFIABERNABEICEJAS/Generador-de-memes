45bc1e12db612e3c5decc1fe2fc455ca05740993
unknown <sofiabernabeicejas@gmail.com>
2021-08-11 18:34:04 -0300

    avanzar con js en formulario texto

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 // boton imagen abrir-cerrar
 let botonImagen =document.getElementById("boton-imagen")
 let seccionImagen=document.getElementById("formulario-imagen")
+let botonTexto = document.getElementById("boton-texto")
+let seccionTexto=document.getElementById("formulario-texto")
+let botonCerrarTexto= document.getElementById("boton-cerrar-texto")
+let botonCerrarImagen = document.getElementById("boton-cerrar-imagen")
+let fondoSuperior =document.getElementById("fondo-superior")
+let fondoInferior = document.getElementById("fondo-inferior")
+let inputFondoColor = document.getElementById("input-fondo-color")
+let checkboxSuperior = document.getElementById("checkbox-superior") 
+let checkboxInferior=document.getElementById("checkbox-inferior")
+let textoSuperior= document.getElementById("texto-superior")
+let inputTextoSuperior = document.getElementById("input-texto-superior")
+let textoInferior= document.getElementById("texto-inferior")
+let inputTextoInferior=document.getElementById("input-texto-inferior")
+let inputTextoColor= document.getElementById("input-texto-color")
 
+let claseOcultar=document.querySelector(".ocultar")
+
+
+
+//IMAGEN
+let inputColorFondoMeme =document.getElementById("input-color-fondo-meme")
+let espacioMeme =document.getElementById("fondo-espacio-meme")
+
+//boton imagen aparecer-desaparecer 
 botonImagen.onclick=()=>{    
 seccionImagen.style.display="flex"
 seccionTexto.style.display="none"
 }
-
-let botonCerrarImagen = document.getElementById("boton-cerrar-imagen")
+//boton cerrar imagen
 
 botonCerrarImagen.onclick=()=>{
  seccionImagen.style.display="none"
@@ -15,15 +37,13 @@ botonCerrarImagen.onclick=()=>{
 }
 
 // boton texto abrir-cerrar
-let botonTexto = document.getElementById("boton-texto")
-let seccionTexto=document.getElementById("formulario-texto")
 
 botonTexto.onclick=()=>{
  seccionTexto.style.display="flex"
  seccionImagen.style.display="none"
 }
- 
-let botonCerrarTexto= document.getElementById("boton-cerrar-texto")
+
+//boton cerrar texto
 
 botonCerrarTexto.onclick=()=>{
   seccionTexto.style.display="none" 
@@ -71,24 +91,105 @@ botonModoClaro.onclick=()=>{
   }
   
 }
-
-let textoSuperior= document.getElementById("texto-superior")
-let inputTextoSuperior = document.getElementById("input-texto-superior")
+ 
+//escribir valor en texto superior
 
 inputTextoSuperior.oninput=()=>{
  textoSuperior.textContent=inputTextoSuperior.value
 }
 
-let textoInferior= document.getElementById("texto-inferior")
-let inputTextoInferior=document.getElementById("input-texto-inferior")
+// escrbir  valor en texto inferior
 
 inputTextoInferior.oninput=()=>{
   textoInferior.textContent=inputTextoInferior.value
 }
 
-let inputTextoColor= document.getElementById("input-texto-color")
 
+//ocultar texto superior
+checkboxSuperior.oninput=()=>{
+  fondoSuperior.classList.toggle("ocultar")
+   
+  }
+  
+ //ocultar texto inferior 
+  checkboxInferior.onclick=()=>{
+    fondoInferior.classList.toggle("ocultar")
+  }
+  
+  //botones para alinear texto izquierda, central, derecha
+
+  let botonTextoIzquierda=document.getElementById("boton-texto-izquierda")
+  let botonTextoCentro=document.getElementById("boton-texto-centro")
+  let botonTextoDerecha=document.getElementById("boton-texto-derecha")
+
+  botonTextoIzquierda.onclick=()=>{
+  
+     textoSuperior.style.textAlign="left"
+     textoInferior.style.textAlign="left"
+    
+  }
+
+  botonTextoCentro.onclick=()=>{
+    textoInferior.style.textAlign="left"
+    textoSuperior.style.textAlign="center"
+
+  }
+  botonTextoDerecha.onclick=()=>{
+    textoSuperior.style.textAlign="right"
+    textoInferior.style.textAlign="right"
+  }
+
+  
+
+ //cambiar color texto
 inputTextoColor.oninput=()=>{
   textoSuperior.style.color=inputTextoColor.value
   textoInferior.style.color=inputTextoColor.value
 }
+
+//cambiar color fondo
+inputFondoColor.oninput=()=>{
+  fondoSuperior.style.backgroundColor=inputFondoColor.value
+  fondoInferior.style.backgroundColor=inputFondoColor.value
+}
+
+
+let selectEstiloLetra = document.getElementById("select-estilo-letra")
+
+
+  console.log("selectEstiloLetra.options")
+
+
+
+
+
+
+
+
+
+//IMAGEN
+
+let inputImagen = document.getElementById("input-imagen")
+let espacioImagen = document.getElementById("espacio-imagen")
+
+
+inputImagen.oninput=()=>{
+
+  espacioImagen.src = inputImagen.value
+}
+
+
+//color del fondo
+
+inputColorFondoMeme.oninput=()=>{
+  espacioMeme.style.backgroundColor= inputColorFondoMeme.value
+}
+
+
+//RANGO
+
+let inputBrillo = document.getElementById("input-brillo")
+
+//inputBrillo.oninput=()=>{
+  //espacioImagen=
+//}
